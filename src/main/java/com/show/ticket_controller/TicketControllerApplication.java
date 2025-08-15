1ae174b7d14f5f1e655cbea06533fdeeef40aea7
GabrielGit10110 <gabrieloliveira13945@gmail.com>
2025-08-15 19:03:57 -0300

    V0.1 - Spring initial file structure

---
+++ src/main/java/com/show/ticket_controller/TicketControllerApplication.java
@@ -0,0 +1,13 @@
+package com.show.ticket_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/show/ticket_controller/TicketControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.show.ticket_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
